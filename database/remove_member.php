3a6f953a067a1ce9a575124c1090d92d1e5871cc
blinnagavci <blinnagavci@gmail.com>
2017-04-06 17:16:03 +0200

    Delete Member

--- database/remove_member.php
+++ database/remove_member.php
@@ -9,7 +9,7 @@ if (isset($_GET['id'])) {
     $photo = mysqli_query($conn, $photosql);
     $row = mysqli_fetch_assoc($photo);
     $photopath = $row['photo'];
-    if (!(unlink("../../repository/member_photos/" . $photopath))) {
+    if (!(unlink("../repository/member_photos/" . $photopath))) {
         die("<script type=text/javascript>window.alert('Photo could not be removed. Please check the member photos directory.')</script>");
     }
     if (!(mysqli_query($conn, $removesql))) {
@@ -21,6 +21,6 @@ if (isset($_GET['id'])) {
         die("Could not remove member: " . mysqli_error($conn));
     }
     mysqli_close($conn);
-    header("refresh: 0; url = ../../search_members");
+    header("refresh: 0; url = ../search_members.php");
 }
 
