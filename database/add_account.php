821ebd81aa59668a8f2c3af687f17e95ecffa7b5
Labian Gashi <labiangashi@gmail.com>
2017-04-08 14:19:48 +0200

    push edit delete

--- database/add_account.php
+++ database/add_account.php
@@ -1,40 +1,31 @@
 <?php
 
 require('db_connect.php');
+$id = ($_POST['id']);
+$username = ($_POST['username']);
+$temporary_password = ($_POST['temporarypassword']);
+$email = ($_POST['email']);
+$accounttype = ($_POST['account_type']);
+$password = sha1($temporary_password);
+$test = ($_POST['test']);
 
-if (isset($_POST['submit'])) {
-    $username = $_POST['account_username'];
-    $temporary_password = ($_POST['account_password']);
-    $email = ($_POST['account_email']);
-    $accounttype = $_POST['account_type'];
-
-    $password = sha1($temporary_password);
-    $uploadedFileName = $_FILES['account_upload']['name'];
-    $temp_name = $_FILES['account_upload']['tmp_name'];
-    $temp = explode(".", $_FILES["account_upload"]["name"]);
-    $getID = mysqli_query($conn, "SELECT id FROM account ORDER BY id DESC");
-    $idRow = mysqli_fetch_row($getID);
-    $newfilename = $idRow[0] + 1 . "_" . $username . '.' . end($temp);
-    if (!($_FILES['account_upload']['name'] == "")) {
-        $sql_account = "INSERT INTO account (username, password, email, admin_status, status, photo)
+if ($test === 'pic') {
+    //we can use temp but we are converting every pic to PNG for less space
+    $newfilename = $id . "_" . $username . "." . "png";
+    move_uploaded_file($_FILES['file']['tmp_name'], '../repository/account_photos/' . $newfilename);
+    $sql_account = "INSERT INTO account (username, password, email, admin_status, status, photo)
 VALUES ('$username', '$password', '$email', '$accounttype', '0', '$newfilename')";
-    } else {
-        $sql_account = "INSERT INTO account (username, password, email, admin_status, status)
+} else {
+    $sql_account = "INSERT INTO account (username, password, email, admin_status, status)
 VALUES ('$username', '$password', '$email', '$accounttype', '0')";
-    }
-    if ($uploadedFileName != '') {
-        $upload_directory = "../repository/account_photos/";
-        move_uploaded_file($temp_name, $upload_directory . $newfilename);
-    }
-    $retval1 = mysqli_query($conn, $sql_account);
-
-    if (!$retval1) {
-        die('Could not enter data to account table' . mysqli_connect_error());
-    } else {
-//        echo "<script type='text/javascript'>window.alert('Account successfully added')</script>";
-    }
-
-    mysqli_close($conn);
-    header("refresh: 0; url = ../accounts.php");
 }
+$retval1 = mysqli_query($conn, $sql_account);
+
+if (!$retval1) {
+    die('Could not enter data to account table' . mysqli_connect_error());
+} else {
+    echo 'success';
+}
+
+mysqli_close($conn);
 
--- database/remove_account.php
+++ database/remove_account.php
@@ -1,19 +1,19 @@
 <?php
 
 require ('db_connect.php');
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
 
     $getAccount = mysqli_query($conn, "SELECT admin_status FROM account WHERE id = '$id'");
     $accountType = mysqli_fetch_row($getAccount);
-    
+
     if ($accountType[0] == 0) {
         $getAll = "SELECT username FROM account WHERE admin_status='0' AND status='0'";
         $result = $conn->query($getAll);
 
         if ($result->num_rows <= 1) {
-            echo "<script type=text/javascript>window.alert('You must have at least one Admin account')</script>";
-            header("refresh: 0; url = ../accounts.php");
+            echo "oneadmin";
+            //header("refresh: 0; url = ../accounts.php");
             exit();
         }
     }
@@ -21,10 +21,10 @@ if (isset($_GET['id'])) {
     $removesql = "UPDATE account SET status='1' WHERE id = '$id'";
 
     if (mysqli_query($conn, $removesql)) {
-//        echo "<script type=text/javascript>window.alert('Account removed successfully.')</script>";
+        echo 'success';
     } else {
         die("Could not remove account: " . mysqli_error($conn));
     }
     mysqli_close($conn);
-    header("refresh: 0; url = ../accounts.php");
+    //header("refresh: 0; url = ../accounts.php");
 }
