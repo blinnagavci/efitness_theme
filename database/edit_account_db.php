f3cca91097861dd012ce5f3b8b5b50c89e083210
LABIAN-PC\Labian <labiangashi>
2017-04-09 19:13:52 +0200

    asd

--- database/edit_account_db.php
+++ database/edit_account_db.php
@@ -1,62 +1,53 @@
 <?php
 
 require('db_connect.php');
-if (isset($_POST['account_edit_submit'])) {
 
-
-    $id = $_POST['test-id'];
-    $username = $_POST['account_username'];
-    $temporary_password = ($_POST['account_password']);
-    $email = ($_POST['account_email']);
-    $accounttypeselect = $_POST['account_type'];
-
-    $getAccountPassword = mysqli_query($conn, "SELECT password FROM account WHERE id = '$id'");
-    $accountPassword = mysqli_fetch_row($getAccountPassword);
-
-    if (!($accountPassword[0] == $temporary_password)) {
-        $password = sha1($temporary_password);
-    } else {
-        $password = $temporary_password;
-    }
-
-
-    $getAccount = mysqli_query($conn, "SELECT admin_status FROM account WHERE id = '$id'");
-    $accountType = mysqli_fetch_row($getAccount);
-
-    if ($accountType[0] == 0) {
-        if ($accounttypeselect == 1) {
-            $getAll = "SELECT username FROM account WHERE admin_status='0' AND status='0'";
-            $result = $conn->query($getAll);
-
-            if ($result->num_rows <= 1) {
-                echo "<script type=text/javascript>window.alert('You must have at least one Admin account')</script>";
-                header("refresh: 0; url = ../accounts.php");
-                exit();
-            }
-        }
-    }
-    if ($_FILES['account_upload']['name'] != '') {
-        $uploadedFileName = $_FILES['account_upload']['name'];
-        $temp_name = $_FILES['account_upload']['tmp_name'];
-        $getID = mysqli_query($conn, "SELECT id FROM member ORDER BY id DESC");
-        $idRow = mysqli_fetch_row($getID);
-        $newfilename = $idRow[0] . "_" . $username . ".png";
-        if ($uploadedFileName != '') {
-            $upload_directory = "../repository/account_photos/";
-            move_uploaded_file($temp_name, $upload_directory . $newfilename);
-        }
-        $sql = "UPDATE account SET username='$username', password='$passwosrd', email='$email', admin_status='$accounttypeselect', photo='$newfilename' WHERE id = $id";
-    } else {
-        
-    }
-    $retval1 = mysqli_query($conn, $sql);
-
-
-    if (!$retval1) {
-        die(mysqli_error($conn));
-    }
-
-    mysqli_close($conn);
-    header("location: ../accounts.php");
+$username = ($_POST['username']);
+$temporarypassword = ($_POST['temporarypassword']);
+$email = ($_POST['email']);
+$id = ($_POST['id']);
+$accounttypeselect = $_POST['account_type'];
+$test = ($_POST['test']);
+if ($test === 'pic') {
+    //we can use temp but we are converting every pic to PNG for less space
+    $newfilename = $id . "_" . $username . "." . "png";
+    move_uploaded_file($_FILES['file']['tmp_name'], '../repository/account_photos/' . $newfilename);
 }
+
+
+$getAccount = mysqli_query($conn, "SELECT admin_status FROM account WHERE id = '$id'");
+$accountType = mysqli_fetch_row($getAccount);
+if ($accountType[0] == 0) {
+    if ($accounttypeselect == 1) {
+        $getAll = "SELECT username FROM account WHERE admin_status='0' AND status='0'";
+        $result = $conn->query($getAll);
+        if ($result->num_rows <= 1) {
+            echo "2";
+            exit();
+        }
+    }
+}
+$getAccountPassword = mysqli_query($conn, "SELECT password FROM account WHERE id = '$id'");
+$accountPassword = mysqli_fetch_row($getAccountPassword);
+
+if (!($accountPassword[0] == $temporarypassword)) {
+    $password = sha1($temporarypassword);
+} else {
+    $password = $temporarypassword;
+}
+if ($test === 'pic') {
+    $sql = "UPDATE account SET username='$username', password='$password', email='$email', admin_status='$accounttypeselect', photo='$newfilename' WHERE id = $id";
+} else {
+    $sql = "UPDATE account SET username='$username', password='$password', email='$email', admin_status='$accounttypeselect' WHERE id = $id";
+}
+
+
+$retval1 = mysqli_query($conn, $sql);
+if (!$retval1) {
+    die(mysqli_error($conn));
+} else {
+    echo "success";
+}
+
+mysqli_close($conn);
 ?>
