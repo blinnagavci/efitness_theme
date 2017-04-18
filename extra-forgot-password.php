5e1a360452c4d8e06b28aac8651d657cd023a46c
Blin Nagavci <blinnagavci@gmail.com>
2017-04-18 17:31:00 +0200

    Sent reset link with ajax edhe error if email doesnt exists

--- extra-forgot-password.php
+++ extra-forgot-password.php
@@ -1,147 +1,145 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="description" content="Neon Admin Panel" />
-	<meta name="author" content="" />
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="E-Fitness Admin Panel" />
+        <meta name="author" content="" />
 
-	<link rel="icon" href="assets/images/favicon.ico">
+        <link rel="icon" href="assets/images/favicon.ico">
 
-	<title>Neon | Forgot Password</title>
+        <title>E-Fitness | Forgot Password</title>
 
-	<link rel="stylesheet" href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
-	<link rel="stylesheet" href="assets/css/font-icons/entypo/css/entypo.css">
-	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
-	<link rel="stylesheet" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" href="assets/css/neon-core.css">
-	<link rel="stylesheet" href="assets/css/neon-theme.css">
-	<link rel="stylesheet" href="assets/css/neon-forms.css">
-	<link rel="stylesheet" href="assets/css/custom.css">
+        <link rel="stylesheet" href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
+        <link rel="stylesheet" href="assets/css/font-icons/entypo/css/entypo.css">
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/neon-core.css">
+        <link rel="stylesheet" href="assets/css/neon-theme.css">
+        <link rel="stylesheet" href="assets/css/neon-forms.css">
+        <link rel="stylesheet" href="assets/css/custom.css">
 
-	<script src="assets/js/jquery-1.11.3.min.js"></script>
-
-	<!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-	
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+        <script src="assets/js/jquery-1.11.3.min.js"></script>
 
 
-</head>
-<body class="page-body login-page login-form-fall" data-url="http://neon.dev">
+    </head>
+    <body class="page-body login-page login-form-fall" data-url="http://neon.dev">
 
 
-<!-- This is needed when you send requests via Ajax -->
-<script type="text/javascript">
-var baseurl = '';
-</script>
-	
-<div class="login-container">
-	
-	<div class="login-header login-caret">
-		
-		<div class="login-content">
-			
-			<a href="index.html" class="logo">
-				<img src="assets/images/logo@2x.png" width="120" alt="" />
-			</a>
-			
-			<p class="description">Enter your email, and we will send you your password.</p>
-			
-			<!-- progress bar indicator -->
-			<div class="login-progressbar-indicator">
-				<h3>43%</h3>
-				<span>logging in...</span>
-			</div>
-		</div>
-		
-	</div>
-	
-	<div class="login-progressbar">
-		<div></div>
-	</div>
-	
-	<div class="login-form">
-		
-		<div class="login-content">
-			
-			<form method="post" role="form" id="form_forgot_password">
-				
-				<div class="form-forgotpassword-success">
-					<i class="entypo-check"></i>
-					<h3>An email with your login info has been sent.</h3>
-				</div>
-				
-				<div class="form-steps">
-					
-					<div class="step current" id="step-1">
-					
-						<div class="form-group">
-							<div class="input-group">
-								<div class="input-group-addon">
-									<i class="entypo-mail"></i>
-								</div>
-								
-								<input type="text" class="form-control" name="email" id="email" placeholder="Email" data-mask="email" autocomplete="off" />
-							</div>
-						</div>
-						
-						<div class="form-group">
-							<button type="submit" class="btn btn-info btn-block btn-login">
-								Request Login Info
-								<i class="entypo-right-open-mini"></i>
-							</button>
-						</div>
-					
-					</div>
-					
-				</div>
-				
-			</form>
-			
-			
-			<div class="login-bottom-links">
-				
-				<a href="extra-login.php" class="link">
-					<i class="entypo-lock"></i>
-					Return to Login Page
-				</a>
-				
-				<br />
-				
-				
-			</div>
-			
-		</div>
-		
-	</div>
-	
-</div>
+        <!-- This is needed when you send requests via Ajax -->
+        <script type="text/javascript">
+            var baseurl = '';
+        </script>
+
+        <div class="login-container">
+
+            <div class="login-header login-caret">
+
+                <div class="login-content">
+
+                    <a href="index.html" class="logo">
+                        <img src="assets/images/logo@2x.png" width="120" alt="" />
+                    </a>
+
+                    <p class="description">Enter your email, and we will send you a reset link.</p>
+
+                    <!-- progress bar indicator -->
+                    <div class="login-progressbar-indicator">
+                        <h3>43%</h3>
+                        <span>sending reset link...</span>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="login-progressbar">
+                <div></div>
+            </div>
+
+            <div class="login-form">
+
+                <div class="login-content">
+
+                    <form method="post" role="form" id="form_forgot_password">
+
+                        <div class="form-forgotpassword-success">
+                            <i class="entypo-check"></i>
+                            <h3>Reset email has been sent</h3>
+                            <p>Please check your email and reset the password </p>
+                        </div>
+
+                        <div class="form-login-error">
+                            <h3>Error</h3>
+                            <p>Something went wrong! Please make sure this email address is valid.</p>
+                        </div>
+
+                        <div class="form-steps">
+
+                            <div class="step current" id="step-1">
+
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon">
+                                            <i class="entypo-mail"></i>
+                                        </div>
+
+                                        <input type="text" class="form-control" name="email" id="email" placeholder="Email" data-mask="email" autocomplete="off" />
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-info btn-block btn-login">
+                                        Proceed
+                                        <i class="entypo-right-open-mini"></i>
+                                    </button>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </form>
 
 
-	<!-- Bottom scripts (common) -->
-	<script src="assets/js/gsap/TweenMax.min.js"></script>
-	<script src="assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
-	<script src="assets/js/bootstrap.js"></script>
-	<script src="assets/js/joinable.js"></script>
-	<script src="assets/js/resizeable.js"></script>
-	<script src="assets/js/neon-api.js"></script>
-	<script src="assets/js/jquery.validate.min.js"></script>
-	<script src="assets/js/neon-forgotpassword.js"></script>
-	<script src="assets/js/jquery.inputmask.bundle.js"></script>
+                    <div class="login-bottom-links">
+
+                        <a href="extra-login.php" class="link">
+                            <i class="entypo-lock"></i>
+                            Return to Log In page
+                        </a>
+
+                        <br />
 
 
-	<!-- JavaScripts initializations and stuff -->
-	<script src="assets/js/neon-custom.js"></script>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
 
 
-	<!-- Demo Settings -->
-	<script src="assets/js/neon-demo.js"></script>
+        <!-- Bottom scripts (common) -->
+        <script src="assets/js/gsap/TweenMax.min.js"></script>
+        <script src="assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+        <script src="assets/js/joinable.js"></script>
+        <script src="assets/js/resizeable.js"></script>
+        <script src="assets/js/neon-api.js"></script>
+        <script src="assets/js/jquery.validate.min.js"></script>
+        <script src="assets/js/neon-forgotpassword.js"></script>
+        <script src="assets/js/jquery.inputmask.bundle.js"></script>
 
-</body>
+
+        <!-- JavaScripts initializations and stuff -->
+        <script src="assets/js/neon-custom.js"></script>
+
+
+        <!-- Demo Settings -->
+        <script src="assets/js/neon-demo.js"></script>
+
+    </body>
 </html>
